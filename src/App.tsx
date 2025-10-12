922fb990418f53e8eb3cf5b99059b4a5e29cb461
S.Arun Saravanan <arunsivagnanamurthy@gmail.com>
2025-10-13 01:57:51 +0530

    Add scroll-to-top behavior: Always start at top of page on refresh/navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import LandingPage from './pages/LandingPage';
 import PredictionPage from './pages/PredictionPage';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen">
         <Toaster position="top-right" />
         <Navbar />
